0753bd59009fddb11d89fc443cfedfd759c27263
ramonnpp <155456459+ramonnpp@users.noreply.github.com>
2024-03-06 15:31:39 +0700

    Add files via upload

---
+++ pertemuan1/pertemuan1/Mahasiswa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemuan1
+{
+    internal class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string NIM { get; set; }
+        public string Jurusan { get; set; }
+        public int Angkatan { get; set; }
+
+        public Mahasiswa(string nama, string nim, string jurusan, int angkatan)
+        {
+            Nama = nama;
+            NIM = nim;
+            Jurusan = jurusan;
+            Angkatan = angkatan;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Nama\t : " + Nama);
+            Console.WriteLine("NIM\t : " + NIM);
+            Console.WriteLine("Jurusan\t : " + Jurusan);
+            Console.WriteLine("Angkatan\t : " + Angkatan);
+            Console.WriteLine();
+        }
+    }
+      
+}
